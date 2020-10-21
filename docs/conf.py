e53db5780d4b8fd82005b2bc6e9ee2537ad04417
Alex Norman <alex96norman@gmail.com>
2020-10-21 12:42:15 +0100

    #7 fixed formatting issues in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ author = 'Matthew Cotton, David Seiferth, Scott Stacey, Alex Norman, Emma Borsie
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-]
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
