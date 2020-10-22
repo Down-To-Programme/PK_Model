01c5fc17d95e7daf244ac8b023abf1f6cc6986cc
scottstacey <scott.stacey@lincoln.ox.ac.uk>
2020-10-22 11:36:27 +0100

    made flake8 compliant

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -12,9 +12,9 @@ class Protocol:
         an example paramter
 
     """
-    def __init__(self, dose_amount, subcutaneous=False, 
-    k_a=1, continuous=False, continuous_period=[0, 0], dose_times=[],
-    multiple=False):
+    def __init__(self, dose_amount, subcutaneous=False,
+                 k_a=1, continuous=False, continuous_period=[0, 0],
+                 dose_times=[], multiple=False):
         self.subcutaneous = subcutaneous
         self.k_a = k_a
         self.dose_amount = dose_amount
@@ -22,26 +22,24 @@ class Protocol:
         self.multiple = multiple
         self.continuous_period = continuous_period
         self.dose_times = dose_times.sort()
-        
 
     def dose(self, dose_amount):
         self.dose_amount = dose_amount
         return dose_amount
 
     def make_subcutaneous(self, k_a):
-            self.k_a = k_a 
-            self.subcutaneous = True
+        self.k_a = k_a
+        self.subcutaneous = True
 
     def make_continuous(self, time_added, time_removed):
         self.continuous = True
         self.continuous_period[0] = time_added
         self.continuous_period[1] = time_removed
-    
+
     def add_dose_time(self, time):
         self.dose_times.append(time)
         self.dose_times.sort()
-    
-    
+
     def dose_time_function(self, t):
         #if self.multiple: #multiple dose function
         #    n = 0
@@ -49,12 +47,11 @@ class Protocol:
         #        if timepoint <= t:
         #            n += 1
         #    dose_t_multiple = n * self.dose_amount
-                    
         if self.continuous:
-            if t <= self.continuous_period[1] and t >= self.continuous_period[0]:
+            if t <= self.continuous_period[1] and \
+               t >= self.continuous_period[0]:
                 dose_t_continuous = self.dose_amount
             else:
                 dose_t_continuous = 0
-                
         dose_t = dose_t_continuous
         return dose_t
