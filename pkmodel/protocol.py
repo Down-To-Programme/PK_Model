74b2f1d45c8c60f0c4faec2637f5332e1c3b5ebc
scottstacey <scott.stacey@lincoln.ox.ac.uk>
2020-10-22 10:31:10 +0100

    First attempt at writing a dosing protocol

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -21,7 +21,7 @@ class Protocol:
         self.continuous = continuous
         self.multiple = multiple
         self.continuous_period = continuous_period
-        self.dose_times = dose_times
+        self.dose_times = dose_times.sort()
         
 
     def dose(self, dose_amount):
@@ -40,3 +40,34 @@ class Protocol:
     def add_dose_time(self, time):
         self.dose_times.append(time)
         self.dose_times.sort()
+    
+    
+    @property
+    def dose_time_function(self, t):
+        if self.multiple: #multiple dose function
+            n = 0
+            for timepoint in self.dose_times:
+                if timepoint <= t:
+                    n += 1
+            dose_t_multiple = n * self.dose_amount
+                    
+        if self.continuous: #continuous time function
+            if t <= self.continuous_period[1] and t >= self.continuous_period[0]:
+                dose_t_continuous = self.dose_amount * t 
+            elif t < self.continuous_period[0]:
+                dose_t_continuous = 0
+            else:
+                dose_t_continuous = self.dose_amount * self.continuous_period[1]
+
+        #Total dose function
+        if self.continuous and self.multiple:
+            dose_t = dose_t_multiple + dose_t_continuous
+        elif self.continuous:
+            dose_t = dose_t_continuous
+        elif self.multiple:
+            dose_t = dose_t_multiple
+        else:
+            dose_t = 0
+        
+        return dose_t
+        
