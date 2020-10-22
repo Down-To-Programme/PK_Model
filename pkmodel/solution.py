dada273e6bc0928ce783812a572daac54813e48a
DSeiferth <david.seiferth@dtc.ox.ac.uk>
2020-10-22 15:18:26 +0100

    added documentation to solution class #33

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -16,6 +16,18 @@ class Solution:
         an example paramter
 
     model: using model class
+        with model( Vc, Vps, Qps, CL)
+
+    protocol: using protocoll class to specify
+        intravenous or subcutaneous dosing
+
+    tmax: float
+        integrates until it reaches tmax
+        default value is 1
+
+    nsteps: int
+        number of integration steps
+        default value is 1000
 
     """
     def __init__(self, model, protocol, tmax=1, nsteps=1000):
@@ -24,32 +36,44 @@ class Solution:
         self.t_eval = np.linspace(0, tmax, nsteps)
         self.solver()
 
-    def rhs(self, t, y):  # intravenous
+    def rhs_intravenous(self, t, y):
+        '''
+        Right hand side of flux equation
+        dimension of system = number of compartments = self.model.size
+        Parameters
+        ----------
+        t: time
+        y: state vector [qc, q_p1, q_p2]
+        '''
         state = y
-        Vc = self.model.Vc
-        CL = self.model.CL
-        cleared = state[0] / Vc * CL
+        Vc = self.model.Vc  # volume of the main compartment
+        CL = self.model.CL  # Clearance rate
+        cleared = state[0] / Vc * CL  # flux going out of the main compartment
         dq_dt = [0]  # [dqc, dq_p1, dq_p2]
 
-        flux_sum = 0
+        flux_sum = 0  # sum of flux between compartments
         # loop over peripheral compartments
         for comp in range(1, self.model.size):
+            # transition rate etween main compartment
+            # and i-th peripheral compartment
             Q_pi = self.model.Qps[comp - 1]
+            # volume of i-th peripheral compartment
             V_pi = self.model.Vps[comp - 1]
+            # flux between peripheral and main compartment
             flux = Q_pi * (state[0] / Vc - state[comp] / V_pi)
             dq_dt.append(flux)
             flux_sum += flux
         dq_dt[0] = self.protocol.dose_time_function(t) - cleared - flux_sum
         return dq_dt
 
-    def rhs_sub(self, t, y):  # subsutaneous
+    def rhs_subcutaneous(self, t, y):  # subcutaneous
         state = y   # one dim more than intravenous
         Vc = self.model.Vc
         CL = self.model.CL
         cleared = state[1] / Vc * CL
         dq_dt = [0, 0]  # [dq0, dqc, dq_p1, dq_p2]
         dq0_dt = self.protocol.dose_time_function(t)
-        - self.protocol.k_a * state[0]
+        dq0_dt -= self.protocol.k_a * state[0]
         dq_dt[0] = dq0_dt
         flux_sum = 0
         # loop over peripheral compartments
@@ -64,11 +88,11 @@ class Solution:
 
     def solver(self):
         if self.protocol.subcutaneous:
-            step_func = self.rhs_sub
+            step_func = self.rhs_subcutaneous
             self.y0 = np.zeros(self.model.size + 1)
             self.sol = np.zeros(self.model.size + 1)
         else:
-            step_func = self.rhs
+            step_func = self.rhs_intravenous
             # only central compartment n=1, one periphal comartment n=2
             # two periphal compartment n=3
             self.y0 = np.zeros(self.model.size)
