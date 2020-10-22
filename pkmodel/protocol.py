d6fc4dd648cfc1f61603fe5d7307ccf7b9e77a4d
scottstacey <scott.stacey@lincoln.ox.ac.uk>
2020-10-22 10:13:16 +0100

    Added multiple dose parameter, added parameters to class call

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -12,13 +12,16 @@ class Protocol:
         an example paramter
 
     """
-    def __init__(self):
-        self.subcutaneous = False
-        self.k_a = 0
-        self.dose_amount = 0
-        self.continuous = False
-        self.contionous_period = [0, 0]
-        self.dose_times = []
+    def __init__(self, dose_amount, subcutaneous=False, 
+    k_a=1, continuous=False, continuous_period=[0, 0], dose_times=[],
+    multiple=False):
+        self.subcutaneous = subcutaneous
+        self.k_a = k_a
+        self.dose_amount = dose_amount
+        self.continuous = continuous
+        self.multiple = multiple
+        self.continuous_period = continuous_period
+        self.dose_times = dose_times
         
 
     def dose(self, dose_amount):
@@ -31,11 +34,9 @@ class Protocol:
 
     def make_continuous(self, time_added, time_removed):
         self.continuous = True
-        self.contionous_period[0] = time_added
-        self.contionous_period[1] = time_removed
+        self.continuous_period[0] = time_added
+        self.continuous_period[1] = time_removed
     
     def add_dose_time(self, time):
         self.dose_times.append(time)
         self.dose_times.sort()
-
-
